6a24fdd0bc516b19ea6fb3e9a49ec6e17ed103c7
Liu, Yuzhe <yliu3529@gatech.edu>
2023-05-10 22:40:57 -0400

    fixed stafic file cannot open

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 RUN pip install gunicorn
+RUN pip install whitenoise
 
 COPY ./app /app
 
--- app/core/settings.py
+++ app/core/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -53,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -121,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = '/static/'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
